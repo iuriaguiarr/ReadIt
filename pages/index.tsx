c65b9655760e1073851a0f3c76595a46a124505a
iuriaguiarr <iuri.aguiarr@gmail.com>
2021-03-03 20:29:44 -0300

    Ajustando propriedades

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,41 @@
-import { useState } from 'react';
-
 import { Button, Container, Flex, Text, Textarea } from '@chakra-ui/react';
+import {
+  BotaoStyle,
+  ContainerStyle,
+  FlexStyle,
+  TextareaStyle,
+  TextStyle,
+} from '../styles/pages/index';
+import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Index() {
   const [text, setText] = useState('');
 
-  function speak() {
+  function sayIt() {
     if (process.browser) {
       const utterance = new SpeechSynthesisUtterance();
       utterance.lang = 'pt-BR';
 
-      utterance.rate = 1.2;
-      utterance.volume = 100;
-      utterance.text = text;
+      utterance.rate = 1.12;
+      utterance.volume = 1;
 
-      speechSynthesis.speak(utterance);
+      return utterance;
+    } else {
+      return new SpeechSynthesisUtterance();
+    }
+  }
+
+  function speak() {
+    if (process.browser) {
+      speechSynthesis.cancel();
+
+      var sentences = text.split('.');
+      for (var i = 0; i < sentences.length; i++) {
+        var toSay = sayIt();
+        toSay.text = sentences[i];
+        speechSynthesis.speak(toSay);
+      }
     }
   }
 
@@ -25,49 +46,22 @@ export default function Index() {
   }
 
   return (
-    <Container
-      display="flex"
-      flexDir="column"
-      justifyContent="center"
-      alignItems="center"
-      width="100vw"
-      height="100vh"
-    >
-      <Text textAlign="left" fontSize="xl" width="50vw">
-        Insira o texto desejado abaixo, e logo em seguida aperte em falar.
+    <Container {...ContainerStyle}>
+      <Head>
+        <title></title>
+      </Head>
+      <Text {...TextStyle}>
+        Insira o texto desejado abaixo, e logo em seguida pressione falar.
       </Text>
 
       <Textarea
-        width="50vw"
-        margin={4}
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        {...TextareaStyle((event) => {
           setText(event.target.value);
-        }}
+        })}
       />
-      <Flex
-        width="50vw"
-        justifyContent="space-between"
-        alignItems="center"
-        gridGap={4}
-      >
-        <Button
-          background="green.500"
-          _hover={{ background: 'green.600' }}
-          color="white"
-          width="100%"
-          onClick={speak}
-        >
-          Falar
-        </Button>
-        <Button
-          background="red.500"
-          _hover={{ background: 'red.600' }}
-          color="white"
-          width="100%"
-          onClick={stop}
-        >
-          Parar
-        </Button>
+      <Flex {...FlexStyle}>
+        <Button {...BotaoStyle('green', speak)}>Falar</Button>
+        <Button {...BotaoStyle('red', stop)}>Parar</Button>
       </Flex>
     </Container>
   );
